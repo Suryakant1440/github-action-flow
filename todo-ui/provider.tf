682439ddc023f0bf1e48dd06ccd46a349172e5a1
Surya Kant Maurya <suryakant.maurya85@gmail.com>
2026-09-22 12:10:18 +0530

    Update provider.tf

--- todo-ui/provider.tf
+++ todo-ui/provider.tf
@@ -1,6 +1,14 @@
-provider "azurerm" {
-features {
-  
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 }
+
+provider "azurerm" {
+  features {}
 subscription_id = "419df3c6-e8dd-4035-b096-c692fe3e62a8"
 }
+
